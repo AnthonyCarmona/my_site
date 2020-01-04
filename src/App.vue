4ce8daedaedd6ecdb0a5ae873bfc97e078dc451a
Anthony Carmona <anthony.carmona07@gmail.com>
2020-01-04 10:38:08 -0600

    added retirement calculator

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div>
-        my projects
-        </div>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create</router-link>|
+      <router-link :to="{ name: 'retirement' }">Calculator</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
 
-export default {
-  name: 'App',
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventCreate from './views/EventCreate.vue'
+import EventList from './views/EventList.vue'
+import EventShow from './views/EventShow.vue'
+import retirement from './views/retirement.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventList
+    },
+    {
+      path: '/event',
+      name: 'event-show',
+      component: EventShow
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    }, 
+    {
+      path: '/retirement',
+      name: 'retirement',
+      component: retirement
+    }
+
+  ]
+})
